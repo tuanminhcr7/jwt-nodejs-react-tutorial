57b13075e025c2206dc40b34ae740f3989351d3d
unknown <minhvnt.hn@havaz.com.vn>
2024-04-25 09:41:55 +0700

    MVC

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,12 @@
 import express from "express";
+import homeController from '../controller/homeController';
 
 const router = express.Router();
 
 const initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send("Hello World");
-    });
+    router.get('/', homeController.handleHelloWorld);
+
+    router.get('/user', homeController.handleUserPage);
 
     return app.use("/", router);
 }
